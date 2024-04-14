f3af0be18ae2cdc1c423bc5a3651fe26622ebebe
Cenk Yenikoylu <mcy@dotnetmcy.net>
2024-04-14 17:40:33 +0300

    published

--- BlazorSignalRApp/obj/Release/net8.0/BlazorSignalRApp.AssemblyInfo.cs
+++ BlazorSignalRApp/obj/Release/net8.0/BlazorSignalRApp.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorSignalRApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b39a2cdc3ec0f5d53fb1077951e4464d21f38fd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorSignalRApp")]
